1a2a7c6cad21e0084f8875a6aaf75b03e1095f07
karimtz999 <farkarim36@gmail.com>
2024-06-13 13:27:40 +0100

    Initial commit

---
+++ Java/Variables1.java
@@ -0,0 +1,18 @@
+public class Variables1 {
+    public static void main(String[] args) {
+      // Student data
+      String studentName = "John Doe";
+      int studentID = 15;
+      int studentAge = 23;
+      float studentFee = 75.25f;
+      char studentGrade = 'B';
+      
+      // Print variables
+      System.out.println("Student name: " + studentName);
+      System.out.println("Student id: " + studentID);
+      System.out.println("Student age: " + studentAge);
+      System.out.println("Student fee: " + studentFee);
+      System.out.println("Student grade: " + studentGrade);
+    }
+  }
+  
