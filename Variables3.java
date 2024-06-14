6081161fd326df3711c225d267a6dc5abf277598
karimtz999 <farkarim36@gmail.com>
2024-06-14 23:41:48 +0100

    update 14.062024

---
+++ Variables3.java
@@ -0,0 +1,16 @@
+public class Variables3 {
+  public static void main(String[] args) {
+    // Create integer variables
+    int length = 4;
+    int width = 6;
+    int area;
+
+    // Calculate the area of a rectangle
+    area = length * width;
+
+    // Print variables
+    System.out.println("Length is: " + length);
+    System.out.println("Width is: " + width);
+    System.out.println("Area of the rectangle is: " + area);
+  }
+}
